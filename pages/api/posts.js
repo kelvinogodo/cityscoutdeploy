a38680521cccc31cea5a70b14278286431ab9acd
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-10-04 17:36:37 -0700

    fixings

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -5,5 +5,6 @@ export default async function handler(req, res) {
     console.log('FETCHING DATA')
     const posts = await Post.find()
     console.log('posts successfully fetched')
+    console.log(posts)
     res.status(200).json(posts)
 }
