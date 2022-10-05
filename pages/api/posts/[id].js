a5d71d678b7f5255df61f0aad38ac6e4c0caa083
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-10-05 13:13:25 -0700

    fixing errors

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -3,12 +3,14 @@ import {connectMongo} from '../../../utils/connectMongo'
 export default async function handler({query:{id}}, res) {
     await connectMongo()
     const posts = await Post.find()
+    if(posts !== null){
     const filteredPost = posts.filter(post =>(post.title.trim() == id ))
-
-    if(filteredPost.length > 0){
-        res.status(200).json(filteredPost[0])
+        if(filteredPost.length > 0){
+            res.status(200).json(filteredPost[0])
+        }
+        else{
+            res.status(404).json({message : `post with the id ${id} does not exist`})
+        }
     }
-    else{
-        res.status(404).json({message : `post with the id ${id} does not exist`})
-    }
+   
 }
--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,10 +1,12 @@
-import Post from '../../models/postModel'
-import {connectMongo} from '../../utils/connectMongo'
+import Post from '../../../models/postModel'
+import {connectMongo} from '../../../utils/connectMongo'
 export default async function handler(req, res) {
     await connectMongo()
     console.log('FETCHING DATA')
+    
     const posts = await Post.find() 
-    // !== null ? res.status(200).json(posts) : res.status(200).json([])
+    if(posts !== null){ res.status(200).json(posts) }
+    else{ res.status(200).json([])}
     console.log('posts successfully fetched')
     console.log(posts)
     return res.status(200).json([]) 
