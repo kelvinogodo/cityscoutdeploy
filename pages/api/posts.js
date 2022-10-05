819b435356a3c680b3078c638ac4dc4ef474f5e9
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-10-04 17:45:26 -0700

    fixings

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -3,8 +3,7 @@ import {connectMongo} from '../../utils/connectMongo'
 export default async function handler(req, res) {
     await connectMongo()
     console.log('FETCHING DATA')
-    const posts = await Post.find()
+    const posts = await Post.find() !== null ? res.status(200).json(posts) : res.status(404).json([])
     console.log('posts successfully fetched')
     console.log(posts)
-    res.status(200).json(posts)
 }
