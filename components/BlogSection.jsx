bab5c8662e0ae55dddea8a224cd8ab726ad86298
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-10-06 11:44:08 -0700

    deployed... finishing touches

--- components/BlogSection.jsx
+++ components/BlogSection.jsx
@@ -2,7 +2,7 @@ import BlogCard from './BlogCard'
 const BlogSection = ({posts}) => {
   return (
     <section className='property-list blog-section'>
-        {Array.isArray(posts) && posts.slice(posts.length - 3, posts.length).reverse().map(
+        {Array.isArray(posts) && posts.reverse().map(
           item =>  <BlogCard key={item._id} item ={item}/>
         )}
     </section>
