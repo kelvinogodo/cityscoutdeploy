0c7d2abb25fe8fe2e6f443d7dafc91060977f0e3
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-10-07 01:07:06 -0700

    deployed... finishing touches

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,43 +1,39 @@
+import nextConnect from 'next-connect';
+import multer from 'multer';
+
 export default async function upload(req, res){
   res.send('im working')
+  const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
+    const upload = multer({
+      storage:storage
+    });
+    
+    apiRoute.use(upload.single('theFiles'));
+    
+    apiRoute.post((req, res) => {
+      res.status(200).json({ data: 'success' });
+    });
+    
+const apiRoute = nextConnect({
+  onError(error, req, res) {
+    res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
 }
-// import nextConnect from 'next-connect';
-// import multer from 'multer';
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, './public/')
-//   },
-//   filename: function (req, file, cb) {
-//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//     cb(null, file.fieldname + '-' + uniqueSuffix)
-//   }
-// })
-// try{const upload = multer({
-//   storage:storage
-// });
-
-// const apiRoute = nextConnect({
-//   onError(error, req, res) {
-//     res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
-//   },
-//   onNoMatch(req, res) {
-//     res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
-//   },
-// });
-
-// apiRoute.use(upload.single('theFiles'));
-
-// apiRoute.post((req, res) => {
-//   res.status(200).json({ data: 'success' });
-// });
-// }catch(err){
-//   res.json(err)
-// }
-// export default apiRoute;
-
-// export const config = {
-//   api: {
-//     bodyParser: false, // Disallow body parsing, consume as stream
-//   },
-// };
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
