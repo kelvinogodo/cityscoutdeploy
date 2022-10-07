1c33fe018d4de99d19767947259561a8e52e1228
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-10-07 01:47:41 -0700

    deployed... finishing touches

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,14 +3,9 @@ import multer from 'multer';
 
 export default async function upload(req, res){
   const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
+    destination: './public/',
+    filename: (req, file, cb) => cb(null, file.originalname),
+  })
     const upload = multer({
       storage:storage
     });
