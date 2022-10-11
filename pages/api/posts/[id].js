704d723b1609523a5ae426c74655d7e0f84e583c
kelvin ogodo <kelvinchukwuebuka385@gmail.com>
2022-10-10 18:26:50 -0700

    deployed... finishing touches

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -4,12 +4,12 @@ export default async function handler({query:{id}}, res) {
     await connectMongo()
     console.log('FETCHING DATA')
     const posts = await Post.find() 
-        const filteredPost = posts.filter(post => post.title == id.toString())
+        const filteredPost = posts.filter(post => post.title.trim() == id)
         if(filteredPost.length > 0){
             res.status(200).json(filteredPost[0])
             console.log(filteredPost)
         }
         else{
-            res.status(404).json({body:'real eatate', image:'realestate (12).jpg', title:'real estate'},filteredPost)
+            res.status(404).json({body:'real eatate', image:'realestate (19).jpg', title:'real estate'},filteredPost)
         }
 }
